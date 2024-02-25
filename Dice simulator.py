b02ef4cfdb33e74c253036cf83309c2e0bf2d858
Abhilasha Sabharwal <86293795+AbhilashaSabharwal@users.noreply.github.com>
2024-02-25 21:48:29 +0530

    Add files via upload

---
+++ Dice simulator.py
@@ -0,0 +1,8 @@
+import time
+import random 
+print('The machine will now simulate a dice!')
+time.sleep(0.5)
+print('Rolling the dice......')
+time.sleep(2)
+print('The dice has been rolled!!')
+print('The dice showed ' + str(random.randint(1,6)))
